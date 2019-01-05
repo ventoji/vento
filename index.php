d597620ad65d51da5b4717983cef3b298c4779ed
Jorge Isaac Vento Maldonado <39380157+ventoji@users.noreply.github.com>
2019-01-05 09:51:01 +0100

    Create index.php
    The required template for wordpress.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// ============================================================================
+// INDEX.PHP
+// -----------------------------------------------------------------------------
+// The main template file. Handles output of pages and posts if a more specific 
+// template file isn't present. Must be present for theme to function properly.
+
+// =============================================================================
+// @package ventodev
+// @version 1.0
+// ==============================================================================
+?>
+
+<?php ventodev_get_template( 'views', 'wp', 'index' ); ?>
